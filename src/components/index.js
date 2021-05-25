ea998f058d62efc549228de665232b8aafdfe160
Robert Dalton <dalton.robertw@gmail.com>
2021-05-25 10:32:38 -0700

    added index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navigation } from "./Navigation";
+export { default as Footer } from "./Footer";
+export { default as Home } from "./Home";
+export { default as About } from "./Businesses";
+export { default as Contact } from "./EndUser";
+export { default as Contact } from "./Visiting";
